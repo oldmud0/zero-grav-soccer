06222ed26f20f6cc6ce5b137f6272afbbb37848c
oldmud0 <oldmud0@users.noreply.github.com>
2017-01-17 22:46:06 -0600

    The dreaded tabs to spaces... also ship invulnerability on respawn

--- entity.py
+++ entity.py
@@ -6,166 +6,170 @@ from map import Map
 from settings import COLLISION_DAMPING, COLLISION_ALGORITHM_EXPERIMENTAL, COLLISION_STUCK_THRESHOLD, COLLISION_UNSTUCK_AGGRESSION
 
 class Entity(pygame.sprite.Sprite):
-	mass = 100
-	
-	collision_counter = 0
-	collision_threshold = COLLISION_STUCK_THRESHOLD
-	
-	x = 0
-	y = 0
-	rot = 0
-	
-	vx = 0
-	vy = 0
-	vrot = 0
+    mass = 100
+    
+    collision_counter = 0
+    collision_threshold = COLLISION_STUCK_THRESHOLD
+    collision_ignore = False
 
-	def __init__(self, path):
-		pygame.sprite.Sprite.__init__(self)
-		
-		print("Loading", path) # Debugging purposes only
-		self.image = pygame.image.load(path).convert_alpha()
-		self.current_sprite = self.main_sprite = self.image
-		
-		self.mask = pygame.mask.from_surface(self.image)
-		
-		self.rect = self.image.get_rect()
-		
-	def respawn(self):
-		self.x = 0
-		self.y = 0
-		self.rot = 0
-		
-		self.vx = 0
-		self.vy = 0
-		self.vrot = 0
-		
-		self.collision_counter = 0
-	
-	def rot_center(self):
-		"""Rotate the entity's sprite while preserving the center.
-		This is done by taking the original sprite's center, rotating the sprite,
-		and then giving the rotated sprite the old center.
-		"""
-		loc = self.rect.center
-		self.image = pygame.transform.rotate(self.current_sprite, self.rot)
-		self.rect = self.image.get_rect()
-		self.rect.center = loc
-	
-	def action(self, delta):
-		self.move()
-		self.collision_detect()
-	
-	def move(self, delta):
-		"""Move a sprite using time-based movement."""
-		ideal_frame_time = 60 # FPS
-		displacement_factor = delta / ideal_frame_time
-				
-		self.x += self.vx * displacement_factor
-		self.y += self.vy * displacement_factor
-		
-		# If we do not round our floats, pygame will floor it for us (bad)
-		self.rect.center = (round(self.x), round(self.y))
-		
-		self.rot = (self.rot + self.vrot) % 360
-		self.rot_center()
-	
-	def collision_detect(self):
-		"""Check for collisions against other entities or the map.
-		Collision detection is very tricky.
-		"""
-		# Check if the collision was with a map
-		self.mask = pygame.mask.from_surface(self.image)
-		point = pygame.sprite.collide_mask(Map.current_map, self)
-		if point:
-			if COLLISION_ALGORITHM_EXPERIMENTAL:
-				self.vx, self.vy = collision.calculate_reflection_angle(Map.current_map.mask, point, (self.vx, self.vy))
-			else: 
-				self.vx, self.vy = collision.simple_collision(Map.current_map.mask, point, (self.vx, self.vy))
-			self.vx, self.vy = self.vx * COLLISION_DAMPING, self.vy * COLLISION_DAMPING
-			
-			self.collision_counter += 1
-			return True
-		return False
-	
-	def unstuck_all(objects):
-		"""Fix all entities that appears to be stuck.
-		If their collision counter is above a certain threshold, they're probably stuck.
-		"""
-		for obj in objects:
-			if obj.collision_counter > obj.collision_threshold:
-				print("Object at", (round(obj.x), round(obj.y)), "is stuck. Trying to unstuck...")
-				obj.unstuck()
-			obj.collision_counter = 0
-	
-	def unstuck(self):
-		"""Unstuck an entity by checking for any open spots we could put it on."""
-		mask = Map.current_map.mask
-		
-		x_max, y_max = mask.get_size()
-		orig_x, orig_y = round(self.x), round(self.y)
-		x, y = orig_x , orig_y
-		unstuck_aggr = COLLISION_UNSTUCK_AGGRESSION
-		
-		# Vertical check for any open spots we could put the entity on...
-		while y > 0:
-			if not mask.get_at((x, y)):
-				self.y = y
-				self.vy = -unstuck_aggr
-				return
-			y -= unstuck_aggr
-		y = orig_y
-		while y < y_max:
-			if not mask.get_at((x, y)):
-				self.y = y
-				self.vy = unstuck_aggr
-				return
-			y += unstuck_aggr
-		y = orig_y
-		
-		# Horizontal spots?
-		while x > 0:
-			if not mask.get_at((x, y)):
-				self.x = x
-				self.vx = -unstuck_aggr
-				return
-			x -= unstuck_aggr
-		x = orig_x
-		while x < x_max:
-			if not mask.get_at((x, y)):
-				self.x = x
-				self.vx = unstuck_aggr
-				return
-			x += unstuck_aggr
-		x = orig_x
-		
-		# Diagonal spots
-		while x > 0 and y > 0:
-			if not mask.get_at((x, y)):
-				self.x, self.y = x, y
-				self.vx, self.vy = -unstuck_aggr, -unstuck_aggr
-				return
-			x, y = x - unstuck_aggr, y - unstuck_aggr
-		x, y = orig_x, orig_y
-		while x < x_max and y < y_max:
-			if not mask.get_at((x, y)):
-				self.x, self.y = x, y
-				self.vx, self.vy = unstuck_aggr, unstuck_aggr
-				return
-			x, y = x + unstuck_aggr, y + unstuck_aggr
-		x, y = orig_x, orig_y
-		while x > 0 and y < y_max:
-			if not mask.get_at((x, y)):
-				self.x, self.y = x, y
-				return
-			x, y = x - unstuck_aggr, y + unstuck_aggr
-		x, y = orig_x, orig_y
-		while x < x_max and y > 0:
-			if not mask.get_at((x, y)):
-				self.x, self.y = x, y
-				return
-			x, y = x + unstuck_aggr, y - unstuck_aggr
-		x, y = orig_x, orig_y
-		
-		# All right, I officially give up now.
-		print("Couldn't unstuck object!")
-	
+    x = 0
+    y = 0
+    rot = 0
+    
+    vx = 0
+    vy = 0
+    vrot = 0
+
+    def __init__(self, path):
+        pygame.sprite.Sprite.__init__(self)
+        
+        print("Loading", path) # Debugging purposes only
+        self.image = pygame.image.load(path).convert_alpha()
+        self.current_sprite = self.main_sprite = self.image
+        
+        self.mask = pygame.mask.from_surface(self.image)
+        
+        self.rect = self.image.get_rect()
+        
+    def respawn(self):
+        self.x = 0
+        self.y = 0
+        self.rot = 0
+        
+        self.vx = 0
+        self.vy = 0
+        self.vrot = 0
+        
+        self.collision_counter = 0
+    
+    def rot_center(self):
+        """Rotate the entity's sprite while preserving the center.
+        This is done by taking the original sprite's center, rotating the sprite,
+        and then giving the rotated sprite the old center.
+        """
+        loc = self.rect.center
+        self.image = pygame.transform.rotate(self.current_sprite, self.rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = loc
+    
+    def action(self, delta):
+        self.move()
+        self.collision_detect()
+    
+    def move(self, delta):
+        """Move a sprite using time-based movement."""
+        ideal_frame_time = 60 # FPS
+        displacement_factor = delta / ideal_frame_time
+                
+        self.x += self.vx * displacement_factor
+        self.y += self.vy * displacement_factor
+        
+        # If we do not round our floats, pygame will floor it for us (bad)
+        self.rect.center = (round(self.x), round(self.y))
+        
+        self.rot = (self.rot + self.vrot) % 360
+        self.rot_center()
+    
+    def collision_detect(self):
+        """Check for collisions against other entities or the map.
+        Collision detection is very tricky.
+        """
+        if self.collision_ignore: 
+            return
+
+        # Check if the collision was with a map
+        self.mask = pygame.mask.from_surface(self.image)
+        point = pygame.sprite.collide_mask(Map.current_map, self)
+        if point:
+            if COLLISION_ALGORITHM_EXPERIMENTAL:
+                self.vx, self.vy = collision.calculate_reflection_angle(Map.current_map.mask, point, (self.vx, self.vy))
+            else: 
+                self.vx, self.vy = collision.simple_collision(Map.current_map.mask, point, (self.vx, self.vy))
+            self.vx, self.vy = self.vx * COLLISION_DAMPING, self.vy * COLLISION_DAMPING
+            
+            self.collision_counter += 1
+            return True
+        return False
+    
+    def unstuck_all(objects):
+        """Fix all entities that appears to be stuck.
+        If their collision counter is above a certain threshold, they're probably stuck.
+        """
+        for obj in objects:
+            if obj.collision_counter > obj.collision_threshold:
+                print("Object at", (round(obj.x), round(obj.y)), "is stuck. Trying to unstuck...")
+                obj.unstuck()
+            obj.collision_counter = 0
+    
+    def unstuck(self):
+        """Unstuck an entity by checking for any open spots we could put it on."""
+        mask = Map.current_map.mask
+        
+        x_max, y_max = mask.get_size()
+        orig_x, orig_y = round(self.x), round(self.y)
+        x, y = orig_x , orig_y
+        unstuck_aggr = COLLISION_UNSTUCK_AGGRESSION
+        
+        # Vertical check for any open spots we could put the entity on...
+        while y > 0:
+            if not mask.get_at((x, y)):
+                self.y = y
+                self.vy = -unstuck_aggr
+                return
+            y -= unstuck_aggr
+        y = orig_y
+        while y < y_max:
+            if not mask.get_at((x, y)):
+                self.y = y
+                self.vy = unstuck_aggr
+                return
+            y += unstuck_aggr
+        y = orig_y
+        
+        # Horizontal spots?
+        while x > 0:
+            if not mask.get_at((x, y)):
+                self.x = x
+                self.vx = -unstuck_aggr
+                return
+            x -= unstuck_aggr
+        x = orig_x
+        while x < x_max:
+            if not mask.get_at((x, y)):
+                self.x = x
+                self.vx = unstuck_aggr
+                return
+            x += unstuck_aggr
+        x = orig_x
+        
+        # Diagonal spots
+        while x > 0 and y > 0:
+            if not mask.get_at((x, y)):
+                self.x, self.y = x, y
+                self.vx, self.vy = -unstuck_aggr, -unstuck_aggr
+                return
+            x, y = x - unstuck_aggr, y - unstuck_aggr
+        x, y = orig_x, orig_y
+        while x < x_max and y < y_max:
+            if not mask.get_at((x, y)):
+                self.x, self.y = x, y
+                self.vx, self.vy = unstuck_aggr, unstuck_aggr
+                return
+            x, y = x + unstuck_aggr, y + unstuck_aggr
+        x, y = orig_x, orig_y
+        while x > 0 and y < y_max:
+            if not mask.get_at((x, y)):
+                self.x, self.y = x, y
+                return
+            x, y = x - unstuck_aggr, y + unstuck_aggr
+        x, y = orig_x, orig_y
+        while x < x_max and y > 0:
+            if not mask.get_at((x, y)):
+                self.x, self.y = x, y
+                return
+            x, y = x + unstuck_aggr, y - unstuck_aggr
+        x, y = orig_x, orig_y
+        
+        # All right, I officially give up now.
+        print("Couldn't unstuck object!")
+    
--- ship.py
+++ ship.py
@@ -7,91 +7,125 @@ from map import Map
 from settings import SHIP_ACCELERATION
 
 class Ship(Entity):
-	mass = 300
-	
-	path_collide_mask = os.path.join("res", "ship-collision.png")
+    mass = 300
+    
+    path_collide_mask = os.path.join("res", "ship-collision.png")
 
-	def __init__(self, team, gamemode):
-		self.team = team
-		self.gamemode = gamemode
-		gamemode.change_team(self, team)
-		
-		if self.team == 0:
-			# blue team faces right
-			self.x = Map.current_map.rect.w * 0.4
-			self.y = Map.current_map.rect.h / 2
-			self.rot = 270
-			path = os.path.join("res", "ship-sprite-blue.png")
-			path_firing = os.path.join("res", "ship-sprite-blue-firing.png")
-		elif self.team == 1:
-			# red team faces left
-			self.x = Map.current_map.rect.w * 0.6
-			self.y = Map.current_map.rect.h / 2
-			self.rot = 90
-			path = os.path.join("res", "ship-sprite-red.png")
-			path_firing = os.path.join("res", "ship-sprite-red-firing.png")
-		else:
-			assert(False)
-			
-		print("Loading", path_firing)
-		self.firing_sprite = pygame.image.load(path_firing).convert_alpha()
-		
-		super(Ship, self).__init__(path)
-		
-		self.mask = pygame.mask.from_surface(pygame.image.load(self.path_collide_mask).convert_alpha())
-		
-		self.thrust	= False
-		self.left = False
-		self.right = False
-		self.grabbing = False
-		
-		self.gamemode = gamemode
-		self.team = team
-		
-		self.collide_sound = pygame.mixer.Sound(os.path.join("res", "ship_bump.wav"))
-		self.collide_with_ship_sound = pygame.mixer.Sound(os.path.join("res", "ship_ship_collision.wav"))
-	
-	def action(self, delta):
-		if self.thrust:
-			self.vx += -SHIP_ACCELERATION*math.sin(math.radians(self.rot))
-			self.vy += -SHIP_ACCELERATION*math.cos(math.radians(self.rot))
-			self.current_sprite = self.firing_sprite
-		else:
-			self.current_sprite = self.main_sprite
-		if self.left:
-			self.vrot = self.vrot / 2 + 2
-		if self.right:
-			self.vrot = self.vrot / 2 - 2
-		if not (self.left ^ self.right):
-			self.vrot = self.vrot / 4
-		if self.grabbing:
-			pass
-		
-		self.move(delta)
-		if self.collision_detect():
-			self.collide_sound.play()
-		if self.collision_detect_others():
-			self.collide_with_ship_sound.play()
-			
-		if self.x < 0 or self.x > Map.current_map.rect.w or self.y < 0 or self.y > Map.current_map.rect.h:
-			self.vx *= -1
-			
-	def respawn(self):
-		super(Ship, self).respawn()
-		self.x, self.y, self.rot = self.gamemode.get_spawn_pos(self)
-		
-	def collision_detect_others(self):
-		teammates = self.gamemode.get_enemies(self)
-		other_objects = [self.gamemode.objective] # It's a list, just for future-proofing
-		
-		objects = teammates + other_objects
-		
-		hit = False
-		for obj in objects:
-			point = pygame.sprite.collide_mask(obj, self)
-			if point:
-				# BONK!
-				collision.elastic_collision(obj, self, point)
-				hit = True
-		return hit
-		
+    def __init__(self, team, gamemode):
+        self.team = team
+        self.gamemode = gamemode
+        gamemode.change_team(self, team)
+        
+        if self.team == 0:
+            # blue team faces right
+            self.x = Map.current_map.rect.w * 0.4
+            self.y = Map.current_map.rect.h / 2
+            self.rot = 270
+            path = os.path.join("res", "ship-sprite-blue.png")
+            path_firing = os.path.join("res", "ship-sprite-blue-firing.png")
+        elif self.team == 1:
+            # red team faces left
+            self.x = Map.current_map.rect.w * 0.6
+            self.y = Map.current_map.rect.h / 2
+            self.rot = 90
+            path = os.path.join("res", "ship-sprite-red.png")
+            path_firing = os.path.join("res", "ship-sprite-red-firing.png")
+        else:
+            assert(False)
+            
+        print("Loading", path_firing)
+        self.firing_sprite = pygame.image.load(path_firing).convert_alpha()
+        
+        super(Ship, self).__init__(path)
+        
+        self.mask = pygame.mask.from_surface(pygame.image.load(self.path_collide_mask).convert_alpha())
+        
+        self.thrust = False
+        self.left = False
+        self.right = False
+        self.grabbing = False
+        
+        self.invuln_timer = 0
+        self.invuln_blink = False
+        self.invuln_blink_timer = 0
+
+        self.gamemode = gamemode
+        self.team = team
+        
+        self.collide_sound = pygame.mixer.Sound(os.path.join("res", "ship_bump.wav"))
+        self.collide_with_ship_sound = pygame.mixer.Sound(os.path.join("res", "ship_ship_collision.wav"))
+    
+    def action(self, delta):
+        if self.thrust:
+            self.vx += -SHIP_ACCELERATION*math.sin(math.radians(self.rot))
+            self.vy += -SHIP_ACCELERATION*math.cos(math.radians(self.rot))
+            self.current_sprite = self.firing_sprite
+        else:
+            self.current_sprite = self.main_sprite
+        if self.left:
+            self.vrot = self.vrot / 2 + 2
+        if self.right:
+            self.vrot = self.vrot / 2 - 2
+        if not (self.left ^ self.right):
+            self.vrot = self.vrot / 4
+        if self.grabbing:
+            pass
+        
+        self.move(delta)
+        if self.collision_detect():
+            self.collide_sound.play()
+        if self.collision_detect_others():
+            self.collide_with_ship_sound.play()
+            
+        if self.x < 0 or self.x > Map.current_map.rect.w or self.y < 0 or self.y > Map.current_map.rect.h:
+            self.vx *= -1
+
+        if self.collision_ignore:
+            self.invuln_timer -= 1
+            self.invuln_blink_timer -= 1
+            if self.invuln_blink_timer == 0:
+                if self.invuln_blink:
+                    self.current_sprite.set_alpha(30)
+                    print("Blink now!",self.invuln_timer)
+                else:
+                    self.current_sprite.set_alpha(255)
+                self.invuln_blink = not self.invuln_blink
+                self.invuln_blink_timer = self.invuln_timer // 30
+            if self.invuln_timer <= 0:
+                self.current_sprite.set_alpha(255)
+                self.make_invulnerable(False)
+
+    def respawn(self):
+        super(Ship, self).respawn()
+        self.x, self.y, self.rot = self.gamemode.get_spawn_pos(self)
+        self.make_invulnerable(True)
+
+    def make_invulnerable(self, invuln):
+        """Make the ship invulnerable"""
+        self.collision_ignore = invuln
+        if invuln:
+            self.invuln_timer = 180
+            self.invuln_blink_timer = 4
+        else:
+            self.invuln_timer = 0
+            self.invuln_blink_timer = 0
+            self.invuln_blink = False
+ 
+    def collision_detect_others(self):
+        if self.collision_ignore: 
+            return
+
+        teammates = self.gamemode.get_enemies(self)
+        other_objects = [self.gamemode.objective]  # It's a list, just for future-proofing
+        
+        objects = teammates + other_objects
+        
+        hit = False
+        for obj in objects:
+            point = pygame.sprite.collide_mask(obj, self)
+            if point:
+                # BONK!
+                collision.elastic_collision(obj, self, point)
+                hit = True
+        return hit
+        
