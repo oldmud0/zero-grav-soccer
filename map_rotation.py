b63f4fd6df549abbfcb1e8dea2b873f17a844053
oldmud0 <oldmud0@users.noreply.github.com>
2017-01-29 22:27:25 -0600

    Add skeleton map rotation file

---
+++ map_rotation.py
@@ -0,0 +1,8 @@
+from soccer import Soccer
+
+class MapRotation:
+    """Defines default map and gamemode rotation."""
+
+    rotation = [
+            ["soccer_arcade1_tilemap", Soccer]
+            ]
