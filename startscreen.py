a7a1ce2242f9391ab8dcd1e783f19776a59ea61c
oldmud0 <oldmud0@users.noreply.github.com>
2017-01-29 22:27:12 -0600

    Add copyright and version to start screen

--- startscreen.py
+++ startscreen.py
@@ -27,11 +27,14 @@ class StartScreen(pygame.surface.Surface):
 
         for button in self.button_list:
             self.button_group.add(button)
-        
+
         self.button_selected.select()
         self.logo = pygame.image.load("res/menu/logo.png").convert_alpha()
         self.background = Stars(size)
         self.instructions = pygame.image.load("res/menu/instructions.png").convert()
+        self.text_font = pygame.font.Font("res/gohufont-11.ttf", 11)
+        self.text_copyright = self.text_font.render("Copyright (c) 2016-2017 Bennett Ramirez", False, (255, 255, 255))
+        self.text_version = self.text_font.render("Pre-alpha 0.1.2", False, (255, 255, 255))
         self.render()
 
     def handle_inputs(self, event):
@@ -55,11 +58,16 @@ class StartScreen(pygame.surface.Surface):
     def render(self):
         """Redraw start screen whenever it changes."""
         self.fill((0,0,0))
-        self.blit(self.background, (0,0))
-        self.button_group.draw(self)
-        self.blit(self.logo, ((self.get_width() - self.logo.get_width()) // 2, round(self.get_height() * .2)))
         if self.instructions_visible:
             self.blit(self.instructions, (0,0))
+        else:
+            self.blit(self.background, (0,0))
+            self.button_group.draw(self)
+            self.blit(self.logo, ((self.get_width() - self.logo.get_width()) // 2, round(self.get_height() * .2)))
+            self.blit(self.text_copyright, ((self.get_width() - self.text_copyright.get_width(), \
+                    self.get_height() - self.text_copyright.get_height())))
+            self.blit(self.text_version, ((0, \
+                    self.get_height() - self.text_version.get_height())))
 
     def show_instructions(self):
         self.instructions_visible = True
